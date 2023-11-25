28ff896d4bc838b08ad64fb149b7a032e3136c5f
root <root@localhost.localdomain>
2023-11-25 15:36:44 -0300

    a

---
+++ vol_persistente/joomla_data/administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2023-11-25 18:32:35 UTC
+#Software: Joomla! 4.4.0 Stable [ Pamoja ] 17-October-2023 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2023-11-25T18:32:35+00:00	INFO 192.168.0.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-11-25T18:32:51+00:00	INFO 192.168.0.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-11-25T18:33:33+00:00	INFO 192.168.0.6	joomlafailure	Username and password do not match or you do not have an account yet.
+2023-11-25T18:33:42+00:00	INFO 192.168.0.6	joomlafailure	Username and password do not match or you do not have an account yet.
